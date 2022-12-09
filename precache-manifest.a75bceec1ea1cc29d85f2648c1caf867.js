cd2f2ccafaf6eb9613e4bdf6eadc7ce31d97c7de
Ria Garg <ria.garg.177@gmail.com>
2022-12-09 14:44:52 +0530

    Updates

---
+++ precache-manifest.a75bceec1ea1cc29d85f2648c1caf867.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f812ac9c111e050e61ba",
+    "url": "/cafe_management/static/css/main.b0c32f29.chunk.css"
+  },
+  {
+    "revision": "f812ac9c111e050e61ba",
+    "url": "/cafe_management/static/js/main.f812ac9c.chunk.js"
+  },
+  {
+    "revision": "4e3eec0efd1bea118b1d",
+    "url": "/cafe_management/static/js/1.4e3eec0e.chunk.js"
+  },
+  {
+    "revision": "a036bd544cf0e127e13c",
+    "url": "/cafe_management/static/css/2.77b2255e.chunk.css"
+  },
+  {
+    "revision": "a036bd544cf0e127e13c",
+    "url": "/cafe_management/static/js/2.a036bd54.chunk.js"
+  },
+  {
+    "revision": "b6f4fcf5e13235f9871a",
+    "url": "/cafe_management/static/js/runtime~main.b6f4fcf5.js"
+  },
+  {
+    "revision": "6edda44805b8743cd6757996554cd5a7",
+    "url": "/cafe_management/static/media/logo2.6edda448.png"
+  },
+  {
+    "revision": "b82213c0c42b529ff8176d6fab9e2baa",
+    "url": "/cafe_management/index.html"
+  }
+];
